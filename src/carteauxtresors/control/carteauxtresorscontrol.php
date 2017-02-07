33aca0a2c0f46f28b592d1870cfa22760e426b73
Hugo <hugo-happe@hotmail.fr>
2017-02-07 10:08:28 +0100

    Fin de la story 3
    Ajout des méthodes concernées dans la vue et le controlleur

--- src/carteauxtresors/control/carteauxtresorscontrol.php
+++ src/carteauxtresors/control/carteauxtresorscontrol.php
@@ -32,7 +32,8 @@ class carteauxtresorscontrol
 
     public function destinationFinale(Request $req, Response $resp, $args)
 	{
-		return (new \carteauxtresors\view\carteauxtresorsview(null))->render('destinationFinale', $req, $resp, $args);
+        $json = \carteauxtresors\model\destination::orderByRaw('RAND()')->take(1)->get()->toJson();
+		return (new \carteauxtresors\view\carteauxtresorsview($json))->render('destinationFinale', $req, $resp, $args);
     }
 
 	

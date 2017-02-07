85d947e429c706f0d5197da3d48c626946c01701
Hugo <hugo-happe@hotmail.fr>
2017-02-07 11:29:48 +0100

    Documentation story 3
    Ajout de la documentation de la story 3

--- api/index.php
+++ api/index.php
@@ -65,8 +65,53 @@ $app->get('/points',
 	}
 )->setName('recupPoints');
 
+/**
+ * @apiGroup Destination
+ * @apiName destinationFinale
+ * @apiVersion 0.1.0
+ *
+ * @api {get} /destination  accès à des ressources destination 
+ *
+ * @apiDescription Retourne un tableau contenant une représentation json d'une destination finale aléatoire.
+ *
+ * @apiSuccess (Succès : 200) {Number} id Identifiant de la destination
+ * @apiSuccess (Succès : 200) {String} nom Nom de la destination 
+ * @apiSuccess (Succès : 200) {Number} latitude Latitude de la destination
+ * @apiSuccess (Succès : 200) {Number} longitude Longitude de la destination
+ * @apiSuccess (Succès : 200) {String} indice1 Indice numéro un de la destination
+ * @apiSuccess (Succès : 200) {String} indice2 Indice numéro deux de la destination
+ * @apiSuccess (Succès : 200) {String} indice3 Indice numéro trois de la destination
+ * @apiSuccess (Succès : 200) {String} indice4 Indice numéro quatre de la destination
+ * @apiSuccess (Succès : 200) {String} indice5 Indice numéro cinq de la destination
+ *
+ * @apiSuccessExample {json} exemple de réponse en cas de succès
+ *     HTTP/1.1 200 OK
+ *	{
+ *		"destination finale": {
+ *			"id": 6,
+ *			"nom": "Cherbourg",
+ *			"latitude": 49.6337308,
+ *			"longitude": -1.622137,
+ *			"indice1": "Une des plus grosses bases de défense",
+ *			"indice2": "Emmanuel Liais",
+ *			"indice3": "La Montagne du Roule",
+ *			"indice4": "Mes parapluies me sont célèbres",
+ *			"indice5": "Cotentin"
+ *		 }
+ *  }
+ *
+ * @apiError (Erreur : 404) RessourceNotFound Commande inexistante
+ *
+ * @apiErrorExample {json} exemple de réponse en cas d'erreur
+ *     HTTP/1.1 404 Not Found
+ *
+ *     {
+ *       "error" : "ressource not found : http://localhost/lbsprive/api/commandes/10"
+ *     }
+ *
+*/
 
-$app->get('/destinations',
+$app->get('/destination',
 	function (Request $req, Response $resp, $args)
 	{
 		return (new carteauxtresors\control\carteauxtresorscontrol($this))->destinationFinale($req, $resp, $args);

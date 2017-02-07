b90fd610371a990d667c91a2fd5ba34f44d7c4bd
Thibaut Dalichampt <thibaut.dalichampt@outlook.com>
2017-02-07 11:35:04 +0100

    Ajout vue scorePartie

--- src/carteauxtresors/view/carteauxtresorsview.php
+++ src/carteauxtresors/view/carteauxtresorsview.php
@@ -25,17 +25,23 @@ class carteauxtresorsview
 		return 400;
 	}
 
-	// -----------
-
-
-	/* private function exemple($req, $resp, $args)
-	{
-		$html = "";
-		return $html;
-    }*/
-
-
 	private function newGame($req, $resp, $args)
+	{
+		if(is_array($this->data))
+		{
+			$json = json_encode($this->data);
+			$resp = $resp->withHeader('Content-Type', 'application/json');
+		}
+		else
+		{
+			$json = $this->data;
+			$resp = $resp->withHeader('Content-Type', 'application/json');
+		}
+		$resp->getBody()->write($json);
+		return $resp;
+	}
+
+		private function scorePartie($req, $resp, $args)
 	{
 		if(is_array($this->data))
 		{
@@ -67,6 +73,6 @@ class carteauxtresorsview
 	{
 		$this->baseURL = $req->getUri()->getBasePath();
 		// Sélectionne automatiquement le sélecteur.
-		$json = $this->$selector($req, $resp, $args);
+		$this->$selector($req, $resp, $args);
 	}
 }

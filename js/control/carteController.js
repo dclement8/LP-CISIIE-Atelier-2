6d49d5ac4e97903210f97a172557bc778d12d511
Charles Piguet <cpiguet@msn.com>
2017-02-10 14:33:01 +0100

    MàJ tableau scores

--- js/control/carteController.js
+++ js/control/carteController.js
@@ -344,14 +344,18 @@ function($scope, $http, leafletMapEvents) {
 	$scope.getBestScores = function() {
 		// Affichage des meilleurs scores
 		$http.get("api/parties").then(function(response) {
-			if(response.status == 200) {
-				$("#tabscores").append("<h2>Tableau des meilleurs scores :</h2><table class='responsive-table'><tr><th>Position</th><th>Pseudo</th><th>Score</th></tr>");
+			if(response.status == 200)
+			{
+				var tab = "<h2>Tableau des meilleurs scores :</h2><table class='responsive-table'><tr><th>Position</th><th>Pseudo</th><th>Score</th></tr>";
 				for(var i = 0; i < response.data.scores.length; i++) {
-					$("#tabscores").append("<tr><td>" + (i + 1) + "</td><td>" + response.data.scores[i].pseudo + "</td><td>" + response.data.scores[i].score + "</td></tr>");
+					tab += "<tr><td>" + (i + 1) + "</td><td>" + response.data.scores[i].pseudo + "</td><td>" + response.data.scores[i].score + "</td></tr>";
 				}
-				$("#tabscores").append("</table>");
+				tab += "</table>";
+				
+				$("#tabscores").html(tab);
 			}
-			else {
+			else
+			{
 				console.log("Erreur : mauvais status http");
 			}
 		}, errorHandler);

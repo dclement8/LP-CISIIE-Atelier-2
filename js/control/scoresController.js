5c7249440ac4d55071731daa4919b6006683099f
hitoshi54 <dylanclement7@gmail.com>
2017-02-10 14:20:16 +0100

    oups .

--- js/control/scoresController.js
+++ js/control/scoresController.js
@@ -1,12 +1,17 @@
 app.controller("scoresController", ["$scope", "$http", "$location",
 function($scope, $http, $location) {
+
     var errorHandler = function(e) {
 		console.log(e);
 	}
 
-            }
-            else {
-                console.log("Erreur : mauvais status http");
+    // Affichage des meilleurs scores
+    var tab = '';
+    $http.get("api/parties").then(function(response) {
+        if(response.status == 200) {
+            tab += "<h2>Tableau des meilleurs scores :</h2><table class='responsive-table'><tr><th>Position</th><th>Pseudo</th><th>Score</th></tr>";
+            for(var i = 0; i < response.data.scores.length; i++) {
+                tab += "<tr><td>" + (i + 1) + "</td><td>" + response.data.scores[i].pseudo + "</td><td>" + response.data.scores[i].score + "</td></tr>";
             }
             tab += "</table>";
             $scope.tabScores = tab;
